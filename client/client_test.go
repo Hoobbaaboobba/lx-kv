89a7eb1a4352ea52850e0167f70385809914f894
Alexey <rc5550463@gmail.com>
2025-03-08 15:38:31 +0300

    add: more tests for the server and client

--- client/client_test.go
+++ client/client_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 )
 
+
 func TestNewClient(t *testing.T) {
     c, err := New("localhost:6379")
     if err != nil {
---
+++ server_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/Hoobbaaboobba/lx-kv/client"
+)
+
+func TestSerevrWithClients(t *testing.T) {
+    server := NewServer(Config{})
+    go func() {
+        log.Fatal(server.Start())
+    }()
+    time.Sleep(time.Second)
+
+    nClients := 10
+    wg := sync.WaitGroup{}
+    wg.Add(nClients)
+    for i := 0; i < nClients; i++ {
+        go func(it int) {
+            c, err := client.New("localhost:6379")
+            if err != nil {
+                log.Fatal(err)
+            }
+            defer c.Close()
+            key := fmt.Sprintf("client_foo_%d", i)
+            value := fmt.Sprintf("client_bar_%d", i)
+            if err := c.Set(context.TODO(), key, value); err != nil {
+                log.Fatal(err)
+            }
+            val, err := c.Get(context.TODO(), key)
+            if err != nil {
+                log.Fatal(err)
+            }
+            fmt.Printf("client %d got this val back => %s\n", i, val)
+            time.Sleep(time.Second)
+            wg.Done()
+        }(i)
+    }
+    wg.Wait()
+
+    time.Sleep(2*time.Second)
+    if len(server.peers) != 0 {
+        t.Fatalf("expected 0 peers, got %d", len(server.peers))
+    }
+}
